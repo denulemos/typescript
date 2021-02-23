b300714c075f38fc3c08198158886aac92284b55
Denisse Lemos <denu.a.lemos@gmail.com>
2021-02-23 19:49:18 -0300

    cambios

---
+++ TipadoDatos.js
@@ -0,0 +1,14 @@
+// En typescript no podemos darle un valor numerico a una variable inicializada como String, en JS si.
+// ----------- Booleans -------------------
+var si = true;
+var no = false;
+// ----------- Number -------------------
+var numero = 5; // No se recomienda emplear var
+var edad = 22; // Constante. No es modificable
+// ----------- String -------------------
+var nombre = "Hola"; // Siempre se usa let para variables. Su valor es modificable.
+// ----------- Array -------------------
+var numeros = [1, 2, 3, 4];
+var nombres = ["1", "2", "3", "4"];
+var coleccionNumeros = [1, 2, 3, 4, 5];
+// ----------- Tupla -------------------
---
+++ TipadoDatos.ts
@@ -0,0 +1,21 @@
+// En typescript no podemos darle un valor numerico a una variable inicializada como String, en JS si.
+
+// ----------- Booleans -------------------
+let si: boolean = true;
+let no: boolean = false;
+
+// ----------- Number -------------------
+var numero = 5; // No se recomienda emplear var
+const edad = 22; // Constante. No es modificable
+
+// ----------- String -------------------
+let nombre: string = "Hola"; // Siempre se usa let para variables. Su valor es modificable.
+
+
+// ----------- Array -------------------
+let numeros: number[] = [1, 2, 3, 4];
+let nombres: string[] = ["1", "2", "3", "4"];
+
+let coleccionNumeros: Array<number> = [1, 2, 3, 4, 5];
+
+// ----------- Tupla -------------------
